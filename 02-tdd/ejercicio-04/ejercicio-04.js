022167273192cdcbd3a4cc9ca12edd869b2ae991
Miriam Mendoza <miriammp1997@gmail.com>
2017-02-27 18:00:49 -0500

    Change function fibonacci

--- 02-tdd/ejercicio-04/ejercicio-04.js
+++ 02-tdd/ejercicio-04/ejercicio-04.js
@@ -1,11 +1,11 @@
 function fibonacci(upperLimit) {
-  var arr = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34];
-  var x = [];
+  var x = [0, 1];
+
     if(typeof upperLimit != 'number' || upperLimit < 0 ){
         return "Error";
     }
-    for(var i = 0 ; i <upperLimit; i ++){
-        x.push(arr[i]);
+    for(var i = 2 ; i <upperLimit; i ++){
+        x.push((i-2)+(i-1));
     } return x;
 }
 
